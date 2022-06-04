da660cf9d50edf99aa071a0938f38702a6239726
Julian01001111 <julianorodrigo534@gmail.com>
2022-06-04 17:16:21 -0300

    new fucntions

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,15 +1,43 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState } from "react";
+import { useEffect } from "react";
+import PageTitle from "../../components/layout/PageTitle";
 
-const UseEffect = (props) => {
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-        </div>
-    )
+function calcParImpar(number) {
+  if (number < 0) return "Negativo";
+  if (number > 20) return "Maior que 20";
+  if (number % 2 === 0) return "Par";
+  return "Impar";
 }
 
-export default UseEffect
+const UseEffect = (props) => {
+  const [number, setNumber] = useState(0);
+  const [parimpar, setParImpar] = useState(0);
+
+  useEffect(
+    function () {
+      setParImpar(calcParImpar(number));
+    },
+    [number]
+  );
+
+  return (
+    <div className="UseEffect">
+      <PageTitle
+        title="Hook UseEffect"
+        subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+      />
+      <div className="center">
+        <label className="text red">{" O número é: " + parimpar + "."}</label>
+
+        <input
+          type="number"
+          className="input"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UseEffect;
--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 const UseState = (props) => {
-    return (
-        <div className="UseState">
-            <PageTitle
-                title="Hook UseState"
-                subtitle="Estado em componentes funcionais!"
-            />
+  const [count, setCount] = useState(0);
+  const [name, setNome] = useState("Juliano")
+  return (
+    <div className="UseState">
+      <PageTitle
+        title="Hook UseState"
+        subtitle="Estado em componentes funcionais!"
+      />
+      <SectionTitle title="Estudos UseStates #01" />
+      <div className="center">
+        <span className="text">{count}</span>
+        <div>
+          <button className="btn" onClick={() => setCount(count - 1)}>
+            -1
+          </button>
+          <button className="btn" onClick={() => setCount(count + 1)}>
+            +1
+          </button>
         </div>
-    )
-}
+      </div>
+      <SectionTitle title="Estudos UseStates #02" />
+        <div className="center">
+            <label className="text">{"O nome atua é: "+name}</label>
+            <input type="text" className="input"
+            value={name} onChange={a => setNome(a.target.value)}>
+            </input>
+        </div>
+    </div>
+  );
+};
 
-export default UseState
+
+export default UseState;
